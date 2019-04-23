2a5b6006c306d1fa351e1ec728d964c3df288f90
Stephan Rajaratnam <Stephan.Rajaratnam@mindgeek.com>
2019-04-23 14:02:45 -0400

    Pre save hooks for avatarImage and hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const md5 = require('md5')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -30,6 +32,39 @@ const userSchema = new mongoose.Schema({
     required: true,
     ref: 'Post'
   }
+})
+
+/**
+ * Pre-save hooks
+ */
+userSchema.pre('save', function(next) { // Add avatar for user
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`
+  next()
+})
+
+userSchema.pre('save', function(next) { // Hash password
+  // Check if password is not modified
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  // Generate salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err)
+    }
+
+    // Generate hash
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) {
+        return next(err)
+      }
+
+      // Set password to generated hash
+      this.password = hash
+      next()
+    })
+  })
 })
 
 module.exports = mongoose.model('User', userSchema)
